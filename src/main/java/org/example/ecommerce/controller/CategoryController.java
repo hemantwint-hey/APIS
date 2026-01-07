9dd9311eb3349fd9c7cbe62343a01644249a2108
hemantwint-hey <hemantwint@gmail.com>
2026-01-07 12:47:32 +0530

    Initial commit

--- src/main/java/org/example/ecommerce/controller/CategoryController.java
+++ src/main/java/org/example/ecommerce/controller/CategoryController.java
@@ -3,11 +3,11 @@ package org.example.ecommerce.controller;
 import org.example.ecommerce.model.Category;
 import org.example.ecommerce.service.CategoryService;
 import org.example.ecommerce.service.CategoryServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.GsonBuilderUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,16 +20,38 @@ public class CategoryController {
     }
 
     @GetMapping("/api/public/categories")
-    public List<Category> getAllCategories(){
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<Category>> getAllCategories(){
+        List<Category> allCategories = categoryService.getAllCategories();
+        return new ResponseEntity<>(allCategories,HttpStatus.OK);
     }
-
-
 
     @PostMapping("/api/public/categories")
-    public String createCtegory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
-        return "Category Added Succesfully";
+        return new ResponseEntity<>("Category Added Succesfully",HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
+    try {
+        String status=categoryService.deleteCategory(categoryId);
+//        return ResponseEntity.status(HttpStatus.OK).body(status);
+//        return ResponseEntity.ok(status);
+        return new ResponseEntity<>(status, HttpStatus.OK);
+    }
+    catch(ResponseStatusException e){
+        return new ResponseEntity<>(e.getReason(),e.getStatusCode());
+        }
+    }
+    @PutMapping("/api/public/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+                                                 @PathVariable Long categoryId ){
+        try{
+            Category savedCategory=categoryService.updateCategory(category,categoryId);
+            return new ResponseEntity<>("Cstegory with category id "+categoryId, HttpStatus.OK);
+
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(),e.getStatusCode());
+        }
+    }
 }
--- src/main/java/org/example/ecommerce/service/CategoryServiceImpl.java
+++ src/main/java/org/example/ecommerce/service/CategoryServiceImpl.java
@@ -1,13 +1,18 @@
 package org.example.ecommerce.service;
 
 import org.example.ecommerce.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories=new ArrayList<>();
+    private long  custonId = 1L;
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -15,6 +20,34 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
-            categories.add(category);
+        category.setCategoryId(custonId++);
+        categories.add(category);
     }
+    @Override
+    public String deleteCategory(Long categoryId){
+
+        Category removed = categories.stream()
+                .filter(c->c.getCategoryId()==categoryId)
+                .findFirst()
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
+
+            categories.remove(removed);
+        return "Category with categoryId " + categoryId + " deleted successfully";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> category1 = categories.stream()
+                .filter(c->c.getCategoryId()==categoryId)
+                .findFirst();
+
+        if(category1.isPresent()){
+            Category existingCategory = category1.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found") ;
+        }
+    }
+
 }
