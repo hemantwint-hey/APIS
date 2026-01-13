ec492de3deeed58dd8bc6b9b5aa8e19fc13270d2
hemantwint-hey <hemantwint@gmail.com>
2026-01-13 18:38:16 +0530

    Initial commit

--- src/main/java/org/example/ecommerce/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/example/ecommerce/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,26 @@
 package org.example.ecommerce.exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException  extends RuntimeException{
+    String resourceName;
+    String field;
+    String fieldName;
+
+    public ResourceNotFoundException() {
+    }
+
+    Long fieldId;
+
+    public ResourceNotFoundException(String resourceName,String field,String fieldName){
+        super(String.format("%s not found with %s: %s",resourceName,field,fieldName));
+        this.resourceName=resourceName;
+        this.field=field;
+        this.fieldName=fieldName;
+    }
+
+    public ResourceNotFoundException( String resourceName, String field, Long fieldId) {
+        super(String.format("%s not found with %s: %d",resourceName,field,fieldId));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldId = fieldId;
+    }
 }
