20cadcdf5e6cad60268d7dbf57082db377f7e660
hemantwint-hey <hemantwint@gmail.com>
2026-06-11 19:33:28 +0530

    Added DTOs

---
+++ src/main/java/org/example/ecommerce/payload/PaymentDTO.java
@@ -0,0 +1,19 @@
+package org.example.ecommerce.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDTO {
+    private Long paymentId;
+    private String paymentMethod;
+    private String pgPaymentId;
+    private String psStatus;
+    private String pgResponseMessage;
+    private String pgName;
+
+
+}
